4f8d8c61eef38f04f59539a27478d260c81791ab
rchertara <32500120+rchertara@users.noreply.github.com>
2018-07-28 16:30:26 -0400

    character methods

--- src/com/company/Character.java
+++ src/com/company/Character.java
@@ -1,11 +1,14 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Character {
 
     private String name;
     private Powers yourPower;
+    boolean inDefense;
     private int energy;
     ArrayList<Items> Inventory;
 
@@ -21,15 +24,59 @@ public class Character {
         return name + " has the power of " + yourPower.getName() + ".";
     }
 
-    public static int Attack(/*monster?*/) {
-        //random damage on enemy; report levels
-        return 0;
+    public void Attack(Monster mon) {
+        inDefense=false;
+        //random damage on enemy; report levels]
+        int randomDam= (int) Math.random(); //confirm what damage range
+        mon.energy=mon.energy-randomDam;
+        System.out.println(name+ " attacked, "+ mon.name+ " has taken"+ randomDam+" points of damage!");
+        }
+
+    public void Defend() {
+        inDefense=true;
+        System.out.println(name+ " is in defense mode ");
+
     }
 
-    public String Flee(/*monster? or takes in energy int?*/) {
+    public void useItem(){
+        inDefense=false;
+        //first show user all items
+        if(getInventory().size()==0){
+            System.out.println("You have no items");
+            return;
+        }
+        else{
+
+            for(int i=0;i<getInventory().size();i++){
+                System.out.println(i+" " + getInventory().get(i).getName());
+            }
+            Scanner scan= new Scanner(System.in);
+            int choice= scan.nextInt();
+
+            while (choice>=getInventory().size()||choice<0){
+                System.out.println("not a valid selection pick again");
+                choice=scan.nextInt();
+            }
+
+            Items item=getInventory().get(choice);
+
+
+
+
+
+
+
+
+        }
+
+    }
+
+
+    public void Flee(/*monster? or takes in energy int?*/) {
         //if energy is too low, provide flee option;
         //also needs to remove one item
-        return null;
+        inDefense=false;
+
     }
 
     public Powers getYourPower() {
